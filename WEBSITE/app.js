a64fbe1f6fad369062ccf2a85362c1cc501cbb3f
rhicsg <rhicsgeonzon158@gmail.com>
2024-10-30 22:49:08 +0800

    continue working on website

---
+++ WEBSITE/app.js
@@ -0,0 +1,28 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+
+    // Wrap around if index is out of bounds
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    // Calculate the new translateX value
+    const offset = -currentSlide * 100;
+    document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+}
+
+function moveSlide(direction) {
+    showSlide(currentSlide + direction);
+}
+
+// Automatically change slide every 5 seconds (optional)
+setInterval(() => {
+    moveSlide(1);
+}, 5000);
